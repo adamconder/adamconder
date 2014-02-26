5e5ed4cf150029fe9f8c68280c32a991198ded26
Adam Conder <adamdconder@gmail.com>
2014-02-26 21:29:04 +0000

    (new) styling fixes

---
+++ beta/system/cms/logs/log-2014-02-26.php
@@ -0,0 +1,154 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-02-26 21:11:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:16:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:18:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:18:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:18:55 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:19:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:19:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:20:35 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:20:46 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:21:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:21:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:22:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:22:05 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:22:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:22:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:22:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:25:15 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:25:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:28:05 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:28:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:29:00 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:29:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:30:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:30:36 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:30:54 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:31:49 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:33:03 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:33:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:35:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:35:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:35:37 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:36:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:37:21 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:37:23 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:37:39 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:40:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:40:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:41:13 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:42:42 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:43:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:44:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:45:42 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:45:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:46:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:46:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:47:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:47:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:52:47 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:53:00 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:53:02 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:53:03 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:53:15 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:54:41 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:55:28 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:55:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:55:55 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:56:28 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:56:31 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:57:30 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:58:22 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:58:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:58:28 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:58:31 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 21:58:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:03:52 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:04:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:04:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:05:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:07:49 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:08:17 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:08:36 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:08:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:09:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:09:04 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:04 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:04 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-02-26 22:09:04 --> Page Missing: addons/default/themes/adamconder/js/hightlight.js/src/styles/default.css
+ERROR - 2014-02-26 22:09:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-02-26 22:09:19 --> Page Missing: addons/default/themes/adamconder/js/hightlight.js/src/styles/default.css
+ERROR - 2014-02-26 22:09:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-02-26 22:09:19 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-02-26 22:09:19 --> Page Missing: addons/default/themes/adamconder/js/hightlight.js/src/styles/default.css
+ERROR - 2014-02-26 22:09:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:10:17 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:11:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:11:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:11:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:11:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:35 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:37 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:48 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:12:49 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:13:05 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:13:31 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:14:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:15:08 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:15:09 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:15:11 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:15:55 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:15:55 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:16:03 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:16:03 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:16:40 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:16:40 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:16:42 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:16:42 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:16:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:16:45 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:17:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:17:04 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:17:05 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:17:05 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:17:52 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:17:52 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:17:54 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:17:54 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:19:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:19:33 --> Severity: Notice  --> Undefined variable: null /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/libraries/Lex/Parser.php(780) : eval()'d code 74
+ERROR - 2014-02-26 22:20:24 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:20:47 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:21:00 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:21:02 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:22:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:22:02 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:22:38 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:22:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:22:57 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:23:15 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:23:47 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:24:08 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:24:10 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:24:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:24:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:24:57 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:25:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:25:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:26:10 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:26:13 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:26:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:02 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:10 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:20 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-02-26 22:27:21 --> Plugin method "options" does not exist on class "Plugin_Theme".
