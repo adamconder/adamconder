95911ec876aaddd9e520a53d29ce9b515406bb7c
Adam Conder <adamdconder@gmail.com>
2014-03-01 15:51:25 +0000

    (new) fixing layout issues with floating for blog titles on mobile devices

--- beta/system/cms/logs/log-2014-03-01.php
+++ beta/system/cms/logs/log-2014-03-01.php
@@ -107,3 +107,10 @@ ERROR - 2014-03-01 16:37:30 --> Plugin method "options" does not exist on class 
 ERROR - 2014-03-01 16:40:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-03-01 16:40:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-03-01 16:43:02 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:44:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:45:47 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:45:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:47:40 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:48:36 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:49:30 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-01 16:50:21 --> Plugin method "options" does not exist on class "Plugin_Theme".
