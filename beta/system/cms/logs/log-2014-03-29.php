d9bcace7c5d7189492330db30c74c5bb5026f5fe
Adam Conder <adamdconder@gmail.com>
2014-03-29 13:21:07 +0000

    (new) adding new contnet to website

--- beta/system/cms/logs/log-2014-03-29.php
+++ beta/system/cms/logs/log-2014-03-29.php
@@ -6,3 +6,64 @@ ERROR - 2014-03-29 11:38:29 --> Plugin method "options" does not exist on class 
 ERROR - 2014-03-29 11:43:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-03-29 11:43:30 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-03-29 11:43:49 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:54:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:55:17 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:57:00 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:02 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:10 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:19 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:21 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:22 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:23 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:25 --> Severity: Notice  --> ob_end_clean(): failed to delete buffer. No buffer to delete /Users/adamconder/Projects/adamconder/adamconder/beta/system/cms/modules/keywords/controllers/admin.php 169
+ERROR - 2014-03-29 11:57:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:59:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:00:03 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:02:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:05:08 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:05:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:06:03 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:07:46 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:08:52 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:10:10 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:10:57 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:12:24 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:12:59 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:13:28 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:15:30 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:15:50 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:15:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:15:59 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:16:20 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:16:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:18:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:18:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:19:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:25:51 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:27:38 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:28:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:29:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:30:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:30:59 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:31:55 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:32:42 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:33:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:34:17 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 12:53:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 13:11:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 13:57:34 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 13:59:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:01:13 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:01:38 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:02:31 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:03:13 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:03:36 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:04:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:06:31 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:07:25 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:11:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:12:21 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:12:34 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:17:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:18:08 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 14:18:53 --> Plugin method "options" does not exist on class "Plugin_Theme".
