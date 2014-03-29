d808e38b539ca1b4b9abf5a05bee7d190d6c105c
Adam Conder <adamdconder@gmail.com>
2014-03-29 10:44:12 +0000

    fix: fixing styles with project layout

---
+++ beta/system/cms/logs/log-2014-03-29.php
@@ -0,0 +1,8 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-03-29 11:32:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:37:54 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:38:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:43:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:43:30 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-03-29 11:43:49 --> Plugin method "options" does not exist on class "Plugin_Theme".
