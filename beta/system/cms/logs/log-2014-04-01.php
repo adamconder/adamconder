8b615777f14f01fb25e57f42fcaea27a1740132e
Adam Conder <adamdconder@gmail.com>
2014-04-01 20:51:53 +0100

    (new) fixng styling issues

---
+++ beta/system/cms/logs/log-2014-04-01.php
@@ -0,0 +1,20 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-01 21:32:22 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:37:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:37:39 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:38:27 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:38:44 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:39:06 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:40:47 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:42:26 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:44:09 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:44:27 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:45:22 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:47:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:49:34 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:50:12 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:50:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:50:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:51:08 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-01 21:51:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
