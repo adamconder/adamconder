94f5b3951832c4972ceb5ae43ec9dadb5568789e
Adam Conder <adamdconder@gmail.com>
2014-04-11 15:24:34 +0100

    (new) added new transaprent header

--- beta/system/cms/logs/log-2014-04-11.php
+++ beta/system/cms/logs/log-2014-04-11.php
@@ -3,3 +3,10 @@
 ERROR - 2014-04-11 11:26:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-04-11 15:55:25 --> Plugin method "options" does not exist on class "Plugin_Theme".
 ERROR - 2014-04-11 15:56:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:57:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:57:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:57:35 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:57:50 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:58:22 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 16:00:55 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 16:18:10 --> Plugin method "options" does not exist on class "Plugin_Theme".
