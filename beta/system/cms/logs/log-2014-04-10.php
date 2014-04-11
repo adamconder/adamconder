f68ff32dc5558019e8b92233ffc211b77ac4c6d1
Adam Conder <adamdconder@gmail.com>
2014-04-11 14:57:25 +0100

    (new) fixing styling issues for header on mobile

---
+++ beta/system/cms/logs/log-2014-04-10.php
@@ -0,0 +1,54 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-10 22:52:20 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:53:06 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:53:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:53:20 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:54:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:54:22 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:57:07 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:57:35 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 22:57:48 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:08:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:00 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:00 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:00 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-04-10 23:09:00 --> Page Missing: addons/default/themes/adamconder/img/adam.png
+ERROR - 2014-04-10 23:09:00 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:20 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:20 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:20 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-04-10 23:09:20 --> Page Missing: addons/default/themes/adamconder/img/adam.png
+ERROR - 2014-04-10 23:09:20 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:32 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:32 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:32 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-04-10 23:09:32 --> Page Missing: addons/default/themes/adamconder/img/adam.png
+ERROR - 2014-04-10 23:09:32 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:54 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:54 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 2
+ERROR - 2014-04-10 23:09:54 --> Severity: Notice  --> Trying to get property of non-object /Users/adamconder/Projects/adamconder/adamconder/beta/addons/default/themes/adamconder/views/modules/pages/page.php 4
+ERROR - 2014-04-10 23:09:54 --> Page Missing: addons/default/themes/adamconder/img/adam.png
+ERROR - 2014-04-10 23:09:54 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:09:57 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:10:29 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:14:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:17:36 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:18:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:18:46 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:19:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:19:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:20:04 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:20:16 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:20:19 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:20:27 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:20:33 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:25:18 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:25:42 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:25:45 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:25:52 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:25:58 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:27:40 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:27:43 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-10 23:35:56 --> Plugin method "options" does not exist on class "Plugin_Theme".
---
+++ beta/system/cms/logs/log-2014-04-11.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2014-04-11 11:26:14 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:55:25 --> Plugin method "options" does not exist on class "Plugin_Theme".
+ERROR - 2014-04-11 15:56:01 --> Plugin method "options" does not exist on class "Plugin_Theme".
